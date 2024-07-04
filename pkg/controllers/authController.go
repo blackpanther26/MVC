e3e657ba3371d89fb71dbd0a77b2456fc68ca41d
blackpanther26 <priyanshuchahal1@gmail.com>
2024-07-04 15:56:36 +0530

    improves structure

--- pkg/controllers/authController.go
+++ pkg/controllers/authController.go
@@ -2,14 +2,9 @@ package controllers
 
 import (
 	"net/http"
-	"os"
 	"time"
-
-	"github.com/blackpanther26/mvc/pkg/config"
-	"github.com/blackpanther26/mvc/pkg/types"
+	"github.com/blackpanther26/mvc/pkg/models"
 	"github.com/blackpanther26/mvc/pkg/utils"
-	"github.com/golang-jwt/jwt/v5"
-	"golang.org/x/crypto/bcrypt"
 )
 
 func SignupPageHandler(w http.ResponseWriter, r *http.Request) {
@@ -44,19 +39,8 @@ func SignupHandler(w http.ResponseWriter, r *http.Request) {
 			return
 		}
 
-		hash, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+		err = models.CreateUser(username, password)
 		if err != nil {
-			utils.RenderTemplate(w, "signup", map[string]interface{}{"ErrorMessage": "Failed to hash password"})
-			return
-		}
-
-		user := types.User{
-			Username:     username,
-			PasswordHash: string(hash),
-		}
-
-		result := config.DB.Create(&user)
-		if result.Error != nil {
 			utils.RenderTemplate(w, "signup", map[string]interface{}{"ErrorMessage": "Failed to create user"})
 			return
 		}
@@ -65,6 +49,56 @@ func SignupHandler(w http.ResponseWriter, r *http.Request) {
 	} else {
 		utils.RenderTemplate(w, "signup", nil)
 	}
+}
+
+func LoginHandler(w http.ResponseWriter, r *http.Request) {
+	var body struct {
+		Username string `json:"username"`
+		Password string `json:"password"`
+	}
+
+	err := r.ParseForm()
+	if err != nil {
+		utils.RenderTemplate(w, "login", map[string]interface{}{"ErrorMessage": "Failed to parse form"})
+		return
+	}
+
+	body.Username = r.FormValue("username")
+	body.Password = r.FormValue("password")
+
+	user, err := models.AuthenticateUser(body.Username, body.Password)
+	if err != nil {
+		utils.RenderTemplate(w, "login", map[string]interface{}{"ErrorMessage": "Invalid username or password"})
+		return
+	}
+
+	token, err := models.GenerateToken(user.ID)
+	if err != nil {
+		http.Error(w, "Failed to create token", http.StatusInternalServerError)
+		return
+	}
+
+	cookie := &http.Cookie{Name: "Authorization", Value: token, Expires: time.Now().Add(24 * time.Hour)}
+	http.SetCookie(w, cookie)
+
+	if user.IsAdmin {
+		http.Redirect(w, r, "/admin", http.StatusSeeOther)
+	} else {
+		http.Redirect(w, r, "/client/", http.StatusSeeOther)
+	}
+}
+
+func LogoutHandler(w http.ResponseWriter, r *http.Request) {
+	http.SetCookie(w, &http.Cookie{
+		Name:     "Authorization",
+		Value:    "",
+		Path:     "/",
+		Expires:  time.Now().Add(-1 * time.Hour * 24),
+		MaxAge:   -1,
+		HttpOnly: true,
+	})
+
+	http.Redirect(w, r, "/login", http.StatusSeeOther)
 }
 
 func isPasswordValid(password string) bool {
@@ -87,73 +121,4 @@ func isPasswordValid(password string) bool {
 	}
 
 	return hasUpperCase && hasLowerCase && hasDigit
-}
-
-func LoginHandler(w http.ResponseWriter, r *http.Request) {
-	var body struct {
-		Username string `json:"username"`
-		Password string `json:"password"`
-	}
-
-	err := r.ParseForm()
-	if err != nil {
-		utils.RenderTemplate(w, "login", map[string]interface{}{"ErrorMessage": "Failed to parse form"})
-		return
-	}
-
-	body.Username = r.FormValue("username")
-	body.Password = r.FormValue("password")
-
-	var user types.User
-	config.DB.First(&user, "username =?", body.Username)
-
-	if user.ID == 0 {
-		utils.RenderTemplate(w, "login", map[string]interface{}{"ErrorMessage": "Invalid username or password"})
-		return
-	}
-
-	err = bcrypt.CompareHashAndPassword([]byte(user.PasswordHash), []byte(body.Password))
-	if err != nil {
-		utils.RenderTemplate(w, "login", map[string]interface{}{"ErrorMessage": "Invalid username or password"})
-		return
-	}
-
-	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
-		"sub": user.ID,
-		"exp": time.Now().Add(time.Hour * 24).Unix(),
-	})
-
-	secret := os.Getenv("SECRET")
-	if secret == "" {
-		http.Error(w, "SECRET environment variable not set", http.StatusInternalServerError)
-		return
-	}
-
-	tokenString, err := token.SignedString([]byte(secret))
-	if err != nil {
-		http.Error(w, "Failed to create token", http.StatusInternalServerError)
-		return
-	}
-
-	cookie := &http.Cookie{Name: "Authorization", Value: tokenString, Expires: time.Now().Add(24 * time.Hour)}
-	http.SetCookie(w, cookie)
-
-	if user.IsAdmin {
-		http.Redirect(w, r, "/admin", http.StatusSeeOther)
-	} else {
-		http.Redirect(w, r, "/client/", http.StatusSeeOther)
-	}
-}
-
-func LogoutHandler(w http.ResponseWriter, r *http.Request) {
-	http.SetCookie(w, &http.Cookie{
-		Name:     "Authorization",
-		Value:    "",
-		Path:     "/",
-		Expires:  time.Now().Add(-1 * time.Hour * 24),
-		MaxAge:   -1,
-		HttpOnly: true,
-	})
-
-	http.Redirect(w, r, "/login", http.StatusSeeOther)
 }
---
+++ pkg/models/authModel.go
@@ -0,0 +1,61 @@
+package models
+
+import (
+	"errors"
+	"os"
+	"time"
+
+	"github.com/blackpanther26/mvc/pkg/config"
+	"github.com/blackpanther26/mvc/pkg/types"
+	"github.com/golang-jwt/jwt/v5"
+	"golang.org/x/crypto/bcrypt"
+)
+
+func CreateUser(username, password string) error {
+	hash, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	if err != nil {
+		return err
+	}
+
+	user := types.User{
+		Username:     username,
+		PasswordHash: string(hash),
+	}
+
+	result := config.DB.Create(&user)
+	return result.Error
+}
+
+func AuthenticateUser(username, password string) (*types.User, error) {
+	var user types.User
+	result := config.DB.First(&user, "username =?", username)
+	if result.Error != nil {
+		return nil, result.Error
+	}
+
+	err := bcrypt.CompareHashAndPassword([]byte(user.PasswordHash), []byte(password))
+	if err != nil {
+		return nil, err
+	}
+
+	return &user, nil
+}
+
+func GenerateToken(userID uint) (string, error) {
+	secret := os.Getenv("SECRET")
+	if secret == "" {
+		return "", errors.New("SECRET environment variable not set")
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+		"sub": userID,
+		"exp": time.Now().Add(time.Hour * 24).Unix(),
+	})
+
+	tokenString, err := token.SignedString([]byte(secret))
+	if err != nil {
+		return "", err
+	}
+
+	return tokenString, nil
+}
